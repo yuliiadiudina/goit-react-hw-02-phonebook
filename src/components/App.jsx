740d7bb5198b532ef6dafa9f249e161c7604f1e6
Yuliia Diudina <yuliiadiudina@gmail.com>
2023-09-05 19:53:40 +0300

    added local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,20 @@ import css from 'components/App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   }
+
+componentDidMount(){
+  const contactsLS = JSON.parse(localStorage.getItem('contacts'));
+  this.setState({contacts: contactsLS});
+}
+
+componentDidUpdate(_, prevState){
+  if( prevState.contacts !== this.state.contacts){
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+}
 
   formSubmit = (data) => {
     const { contacts } = this.state;
