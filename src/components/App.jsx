4dca64b7365470c6dc16e10452d2184ae820048a
Yuliia Diudina <yuliiadiudina@gmail.com>
2023-09-06 18:16:03 +0300

    bugs fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,21 +7,15 @@ import css from 'components/App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [      
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
     filter: '',
   }
-
-componentDidMount(){
-  const contactsLS = JSON.parse(localStorage.getItem('contacts'));
-  this.setState({contacts: contactsLS});
-}
-
-componentDidUpdate(_, prevState){
-  if( prevState.contacts !== this.state.contacts){
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  }
-}
-
+  
   formSubmit = (data) => {
     const { contacts } = this.state;
     if (contacts.some(contact => contact.name === data.name)) {
